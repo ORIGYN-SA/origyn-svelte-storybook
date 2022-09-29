d2f93f8e915d0ff57a9bbf5fb8af4d5265897a7a
fermartz <fernandomvc@gmail.com>
2022-09-29 10:29:55 -0500

    first commit skeleton app

---
+++ .storybook/main.cjs
@@ -0,0 +1,41 @@
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: '@storybook/svelte',
+    core: {
+        builder: '@storybook/builder-vite',
+    },
+    svelteOptions: {
+        preprocess: import('../svelte.config.js').preprocess,
+    },
+    svelteOptions: {
+        preprocess: sveltePreprocess({
+            babel: {
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            loose: true,
+                            modules: false,
+                            targets: {
+                                esmodules: true,
+                            },
+                        },
+                    ],
+                ],
+            },
+        }),
+    },
+    features: {
+        storyStoreV7: true,
+    },
+};
---
+++ src/lib/card/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let title;
+    export let content;
+</script>
+
+<div>
+    {title}
+    {content}
+</div>

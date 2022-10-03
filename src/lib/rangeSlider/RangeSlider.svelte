237652788a72a006430d4dc634f89b8116431e8f
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-10-03 16:47:48 +0200

    FirstCommit

---
+++ src/lib/rangeSlider/RangeSlider.svelte
@@ -0,0 +1,112 @@
+<div class="slider-parent">
+  <output class="value-thumb" ><p>Empty</p></output>
+  <input
+  type="range"
+  />
+</div>
+<style>
+  .slider-parent {
+    position: relative;
+    width: 100%;
+  }
+  .value-thumb {
+    display: none;
+    font-family: "Montserrat";
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0px 4px 0px 4px;
+    min-width: 32px;
+    width: fit-content;
+    height: 22px;
+    background: #151515;
+    border-radius: 999px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 20px;
+    text-align: center;
+    color: #fefefe;
+    padding-top: 2px;
+    position: absolute;
+    left: 0px;
+  }
+  .range-input {
+    -webkit-appearance: none;
+    margin-top: 37px;
+    height: 2px;
+    outline: none;
+    background: rgba(154, 154, 154, 0.3);
+    background-image: linear-gradient(#151515, #151515);
+    border-radius: 1px;
+    background-repeat: no-repeat;
+  }
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 12px;
+    height: 12px;
+    background: #151515;
+    border-radius: 50%;
+    border: none;
+  }
+  ::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+    background: transparent;
+  }
+  ::-webkit-slider-thumb:hover {
+    box-shadow: 0 0 0 6px rgba(21, 21, 21, 0.08);
+  }
+  ::-webkit-slider-thumb:active {
+    box-shadow: 0 0 0 6px rgba(21, 21, 21, 0.16);
+    transition: box-shadow 350ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      left 350ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      bottom 350ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  }
+
+  .range-input:disabled {
+    -webkit-appearance: none;
+    margin-top: 37px;
+    height: 2px;
+    outline: none;
+    background-image: linear-gradient(#9a9a9a, #9a9a9a);
+    border-radius: 1px;
+    background-repeat: no-repeat;
+  }
+
+  ::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+    background: transparent;
+  }
+  ::-webkit-slider-thumb {
+    box-shadow: none;
+    -webkit-appearance: none;
+    background: #9a9a9a;
+  }
+  ::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+  }
+
+  .value-thumb:disabled {
+    display: none;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    min-width: 32px;
+    width: fit-content;
+    height: 22px;
+    background: #151515;
+    border-radius: 999px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 20px;
+    text-align: center;
+    color: #fefefe;
+    position: absolute;
+    left: 0px;
+  }
+</style>
---
+++ src/stories/RangeSlider.stories.js
@@ -0,0 +1,18 @@
+
+import RangeSlider from '../lib/rangeSlider/RangeSliderProcess.svelte';
+
+
+export default {
+  title: 'Component/RangeSlider',
+  component: RangeSlider,
+};
+
+// More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args
+const Template = () => ({
+  Component: RangeSlider
+});
+
+// More on args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary = Template.bind({});
+
+
